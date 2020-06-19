544c0386e82cd71ccd782729d8e1ec0b10cc42a3
Chris Warrick <kwpolska@gmail.com>
2020-06-19 13:29:15 +0200

    find_pwnage: improve logic

--- find_pwnage.py
+++ find_pwnage.py
@@ -7,34 +7,54 @@
 import csv
 import hashlib
 import requests
+import collections
 import time
 import sys
 
-found_breaches = 0
+found_breaches = []
+hash_sets = collections.defaultdict(set)
+hash_values = collections.defaultdict(list)
+session = requests.Session()
 
 
-def progress(char='.'):
+def progress(char="."):
     sys.stderr.write(char)
     sys.stderr.flush()
+
+
+def print_breaches(full_hash):
+    print(full_hash)
+    for t, p in hash_values[full_hash]:
+        print("\t{}\t{}".format(t, p))
 
 
 with open("passwords.csv") as fh:
     data = csv.DictReader(fh)
 
     for line in data:
-        title = line['Title']
-        password = line['Password']
-        full_hash = hashlib.sha1(password.encode('utf-8')).hexdigest()
+        title = line["Title"]
+        password = line["Password"]
+        full_hash = hashlib.sha1(password.encode("utf-8")).hexdigest().upper()
         hash_first, hash_last = full_hash[:5], full_hash[5:]
-        r = requests.get("https://api.pwnedpasswords.com/range/" + hash_first)
-        if hash_last in r.text:
-            progress('!\n')
-            print(title, password)
-            found_breaches += 1
-        else:
-            progress('.')
-        time.sleep(1.6)
+        hash_sets[hash_first].add(hash_last)
+        hash_values[full_hash].append((title, password))
+    title = password = None
+    all_count = sum(len(s) for s in hash_sets.values())
+    print("Checking", all_count, "passwords...")
+    for hash_first, hash_lasts in hash_sets.items():
+        r = session.get("https://api.pwnedpasswords.com/range/" + hash_first)
+        for hash_last in hash_lasts:
+            if hash_last in r.text:
+                progress("!\n")
+                full_hash = hash_first + hash_last
+                print_breaches(full_hash)
+                found_breaches.append(full_hash)
+            else:
+                progress(".")
+        # The API does not require rate-limiting, so we can query it with full speed.
+        # time.sleep(0.05)
 
-print("\nFound", found_breaches, "breaches.")
-sys.exit(found_breaches)
-
+print("\nFound", len(found_breaches), "breaches.")
+for full_hash in found_breaches:
+    print_breaches(full_hash)
+sys.exit(len(found_breaches))
